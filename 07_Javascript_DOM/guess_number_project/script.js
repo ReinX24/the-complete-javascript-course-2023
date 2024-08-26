781cec23cb8bd86edd7cc8ba53dac4c30ebb3c10
Rein <reinX244@gmail.com>
2024-08-26 22:38:37 +0800

    Finished guess_number_project.

--- 07_Javascript_DOM/guess_number_project/script.js
+++ 07_Javascript_DOM/guess_number_project/script.js
@@ -1,9 +1,104 @@
 'use strict';
-// TODO: resume @08 Manipulating CSS Styles
+
+// TODO: resume @12 PROJECT #2 Modal Window
+
+// Evaluates the game
+const evaluateGame = () => {
+	// When there is no input, show warning message and terminate script
+	if (!guessNumberElement.value) {
+		// messageElement.textContent = '❗ No number entered!';
+		displayMessage('❗ No number entered!');
+		return; // terminate rest of the function
+	}
+
+	// Getting guessed number
+	guessNumber = Number(guessNumberElement.value);
+
+	// If the user guesses the correct number
+	if (guessNumber === randomNumber) {
+		updateOnWin();
+	} else {
+		updateOnLoss(); // incorrect guess
+	}
+
+	console.log(guessNumber, randomNumber);
+};
+
+// When the guess is right
+const updateOnWin = () => {
+	if (currentScore > highScore) {
+		highScore = currentScore;
+		highScoreElement.textContent = highScore;
+	}
+
+	randomNumberElement.textContent = randomNumber;
+	randomNumberElement.style.width = '30rem';
+
+	// messageElement.textContent = '🎉 Correct number!';
+	displayMessage('🎉 Correct number!');
+	checkBtn.disabled = true;
+	body.style.backgroundColor = '#60b347'; // background color
+};
+
+// When the guess is wrong
+const updateOnLoss = () => {
+	// Shows message if the guess is too low or too high
+	// messageElement.textContent =
+	// 	guessNumber > randomNumber ? '📈 Too high!' : '📉 Too low!';
+
+	displayMessage(guessNumber > randomNumber ? '📈 Too high!' : '📉 Too low!');
+
+	currentScore--;
+
+	// Update the current score on the page
+	scoreElement.textContent = currentScore;
+
+	// If the user score reaches 0, show lose message
+	if (currentScore == 0) {
+		// Show the random number after losing
+		randomNumberElement.textContent = randomNumber;
+		randomNumberElement.style.width = '30rem';
+
+		// messageElement.textContent = '💥 You lose!';
+		displayMessage('💥 You lose!');
+		checkBtn.disabled = true;
+		body.style.backgroundColor = '#CC3333';
+	}
+};
+
+// Restart the game
+const resetGame = () => {
+	randomNumber = generateRandomNumber(MIN, MAX); // generates new random number
+
+	currentScore = 20; // resetting the score
+	scoreElement.innerHTML = currentScore;
+
+	guessNumber = null;
+	guessNumberElement.value = '';
+
+	// messageElement.textContent = 'Start guessing...';
+	displayMessage('Start guessing...');
+	body.style.backgroundColor = '#222';
+
+	randomNumberElement.textContent = '?';
+	randomNumberElement.style.width = '15rem';
+
+	checkBtn.disabled = false; // enable the checkBtn
+};
+
+// Generate a random number between two numbers
+const generateRandomNumber = (min, max) => {
+	return Math.round(Math.random() * (max - min) + min);
+};
+
+const displayMessage = (message) => {
+	messageElement.textContent = message;
+};
 
 //* Getting the elements from our HTML
 const checkBtn = document.querySelector('#checkBtn');
 const againBtn = document.querySelector('#againBtn');
+const randomNumberElement = document.querySelector('#randomNumber');
 const guessNumberElement = document.querySelector('#guessNumber');
 const scoreElement = document.querySelector('#score');
 const highScoreElement = document.querySelector('#highscore');
@@ -14,80 +109,10 @@ const MIN = 1;
 const MAX = 20;
 
 let currentScore = 20;
-let randomNumber = null;
+let randomNumber = generateRandomNumber(MIN, MAX);
 let highScore = 0;
 let guessNumber = null;
-
-// Generate a random number between two numbers
-const generateRandomNumber = (min, max) => {
-	randomNumber = Math.round(Math.random() * (max - min) + min);
-};
-
-// Evaluates the game
-const evaluateGame = () => {
-	if (!guessNumberElement.value) {
-		messageElement.textContent = '❗ No number entered!';
-		return; // terminate rest of the function
-	}
-
-	// Getting guessed number
-	guessNumber = Number(guessNumberElement.value);
-
-	if (guessNumber === randomNumber) {
-		updateOnWin();
-	} else {
-		updateOnLoss();
-	}
-
-	console.log(guessNumber, randomNumber);
-};
-
-const updateOnLoss = () => {
-	if (guessNumber > randomNumber) {
-		messageElement.textContent = '📈 Too high!';
-	} else if (guessNumber < randomNumber) {
-		messageElement.textContent = '📉 Too low!';
-	}
-
-	currentScore--;
-
-	// Update the current score on the page
-	scoreElement.textContent = currentScore;
-
-	// If the user score reaches 0, show lose message
-	if (currentScore == 0) {
-		messageElement.textContent = '💥 You lose!';
-		checkBtn.disabled = true;
-		body.style.backgroundColor = '#CC3333';
-	}
-};
-
-const updateOnWin = () => {
-	if (currentScore > highScore) {
-		highScore = currentScore;
-		highScoreElement.textContent = highScore;
-	}
-	messageElement.textContent = '🎉 Correct number!';
-	checkBtn.disabled = true;
-	body.style.backgroundColor = '#006633';
-};
-
-// Restart the game
-const resetGame = () => {
-	generateRandomNumber(MIN, MAX);
-
-	currentScore = 20;
-	scoreElement.innerHTML = currentScore;
-	guessNumber = null;
-
-	guessNumberElement.value = '';
-	body.style.backgroundColor = '#222';
-	checkBtn.disabled = false;
-};
 
 // Button triggers
 checkBtn.onclick = evaluateGame;
 againBtn.onclick = resetGame;
-
-// When the page is loaded, generate a random number between 1 and 20
-window.onload = generateRandomNumber(MIN, MAX);
